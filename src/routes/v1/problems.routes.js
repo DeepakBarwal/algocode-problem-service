26e1e3c1d07c0b93e782ecef8d832c328194f04e
Deepak Barwal <deepakbarwal98@gmail.com>
2024-10-13 18:08:53 +0530

    feat: added routing structure for versioned api for problems

---
+++ src/routes/v1/problems.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const { ProblemsController } = require("../../controllers");
+
+const problemRouter = express.Router();
+
+problemRouter.get("/ping", ProblemsController.pingProblemController);
+
+problemRouter.get("/:id", ProblemsController.getProblem);
+
+problemRouter.get("/", ProblemsController.getProblems);
+
+problemRouter.post("/", ProblemsController.addProblem);
+
+problemRouter.delete("/:id", ProblemsController.deleteProblem);
+
+problemRouter.put("/:id", ProblemsController.updateProblem);
+
+module.exports = problemRouter;
